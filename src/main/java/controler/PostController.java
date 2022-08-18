fa05ff360fc2f48b99c3ac9afb33cad5c9727b02
AdamSkrzypek <adam.kamil.skrzypek@gmail.com>
2022-08-18 22:47:50 +0200

    Refactor: - change maven build properties - change order of initializations

--- src/main/java/controler/PostController.java
+++ src/main/java/controler/PostController.java
@@ -8,34 +8,29 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import services.JsonObtainerService;
 import services.PostPersistenceService;
-
 import java.util.List;
 import java.util.function.Supplier;
 
 public class PostController implements JsonController<Post> {
 
-    private JsonObtainerService jsonObtainerService;
+    private JsonObtainerService  jsonObtainerService =  new JsonObtainerService(ArgumentConverter.getUrl());
     private Supplier<String> supplier =  jsonObtainerService::getJsonFromURLAsString;
-    private Converter<Post> postConverter;
-    private PostPersistenceService postPersistenceService;
+    private Converter<Post>  postConverter = new PostConverter(supplier);
+    private PostPersistenceService postPersistenceService = new PostPersistenceService(postConverter);
     private final static Logger logger = LoggerFactory.getLogger(PostController.class);
 
 
     public PostController() {
         //C.
     }
-
-
     @Override
     public Supplier<String> fetchAllPosts() {
-      jsonObtainerService =  new JsonObtainerService(ArgumentConverter.getUrl());
       logger.info("sending request to JsonObtainerService");
       return supplier;
 
     }
     @Override
     public List<Post> convertToObject() {
-        postConverter = new PostConverter(supplier);
         logger.info("sending request to convert raw JSON's to Post objects");
         postConverter.convertToCorrectObject(supplier,Post.class);
         return postConverter.getPostList();
@@ -43,10 +38,8 @@ public class PostController implements JsonController<Post> {
 
     @Override
     public void persistAllPosts() {
-        postPersistenceService = new PostPersistenceService(postConverter);
         logger.info("sending request to PostPersistenceService");
         postPersistenceService.saveAllPostsToSingleFile(ArgumentConverter.getDirectory());
-
     }
 
 }
